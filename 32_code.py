b6596250f744a4bdaaecb4f3c2cad7d348589f68
MrBean1512 <davidmartin1512@gmail.com>
2021-04-14 11:01:19 -0700

    32 completed

--- 32_code.py
+++ 32_code.py
@@ -5,19 +5,29 @@ for line in file:
     lines.append("{}".format(line.strip()))
 file.close()
 
-k = line[0]
-dna = line[1]
+k = int(lines[0])
+dna = lines[1]
 
 def deBruijnGraph(k, dna):
+    #this is the main function
     kDict = {}
-    for i in range(0, len(dna) - k):
+    for i in range(0, len(dna) - k + 1):
+        print(dna[i:i+k-1])
         if dna[i:i+k-1] not in kDict:
-            kDict[dna[i:i+k-1]] = dna[i+1:i+k]
-    kDict.sort()
-    print(kDict)
-    return
+            kDict[dna[i:i+k-1]] = [dna[i+1:i+k]]
+        else:
+            kDict[dna[i:i+k-1]].append(dna[i+1:i+k])    
+    return kDict
 
-answer = deBruijnGraph(k, dna)
+def parse(kDict):
+    parse = []
+    for key, value in kDict.items():
+        parsedList = ",".join(str(x) for x in value)
+        parse.append(key + " -> " + parsedList)
+    parse.sort()
+    return parse
+
+answer = parse(deBruijnGraph(k, dna))
 print(answer)
 fileout = open('32_sub.txt', 'w')
 for i in answer:
